844f9291f19ef5c358e99f2bd37ef09c0428b1c5
Knrc <kontakt.mikey@gmail.com>
2020-06-22 14:48:31 +0200

    Prepared sb!dev command

--- commands/commands.js
+++ commands/commands.js
@@ -9,7 +9,8 @@ exports.run = (bot, message, args, command) => {
     .addField("sb!ping", "Shows your current ping between Server and Client")
     .addField("sb!Social", "Shows the Social Media Platforms from the Developer")
 
-    //.addField("!sbDev", "Send a Message to the Developer with '!sbDev (Message)' you would get an answer from the Dev after reviewing your message ")
+    //.addField("sb!Dev", "Send a Message to the Developer with '!sbDev (Message)' you would get an answer from the Dev after reviewing your message ")
+    //.addField("")
 
     .setTimestamp()
     .setFooter('©Knrc 2020', "https://scontent-ham3-1.cdninstagram.com/v/t51.2885-19/s150x150/79216602_300874060929220_7583700574332256256_n.jpg?_nc_ht=scontent-ham3-1.cdninstagram.com&_nc_ohc=zYeGby5PA1wAX9uYufm&oh=546f142ae9d85f59f01840cf9ca40ef9&oe=5F156236");
